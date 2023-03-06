2d934b33250ade8a227f57c1605c5eff92548b5e
AisauleS <aisaulesibatova@gmail.com>
2023-03-06 12:40:02 +0500

    Added details view when cell tapped

---
+++ Settings/DetailView.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Settings
+//
+//  Created by Aisaule Sibatova on 06.03.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
